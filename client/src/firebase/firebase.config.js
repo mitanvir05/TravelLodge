aae71d1e44d442bb98ecc1f3033f3f11ac64c59a
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-16 22:58:03 +0600

    project setuo from template

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVIw1JDInoY9x6PME4Tu4EIAkWEDSL9js",
+  authDomain: "travel-lodge-86e11.firebaseapp.com",
+  projectId: "travel-lodge-86e11",
+  storageBucket: "travel-lodge-86e11.appspot.com",
+  messagingSenderId: "47303187951",
+  appId: "1:47303187951:web:6f6f3248e7f6652b072a1a",
+  measurementId: "G-WNVM1TX4D4"
+};
+export const app = initializeApp(firebaseConfig)
