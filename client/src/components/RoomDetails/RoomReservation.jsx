560334ef9f2db8b95aebb4414fde98af5479e139
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-17 14:32:30 +0600

    calender added

---
+++ client/src/components/RoomDetails/RoomReservation.jsx
@@ -0,0 +1,12 @@
+
+import Calender from "./Calender";
+
+const RoomReservation = () => {
+    return (
+        <div className="">
+            <Calender/>
+        </div>
+    );
+};
+
+export default RoomReservation;
--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import Header from "../../components/RoomDetails/Header";
 import RoomInfo from "../../components/RoomDetails/RoomInfo";
+import RoomReservation from "../../components/RoomDetails/RoomReservation";
 
 const RoomDetails = () => {
     const { id } = useParams()
@@ -30,13 +31,16 @@ const RoomDetails = () => {
             <Helmet>
                 <title>{room?.title}</title>
             </Helmet>
-            <div className="">
+            <div className=" max-w-screen-lg mx-auto">
 
                 <div className="flex flex-col gap-6">
                     <Header room={room} />
                 </div>
-                <div className="">
-                    <RoomInfo room={room}/>
+                <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                    <RoomInfo room={room} />
+                    <div className="md:col-span-3 order-first md:order-last mb-10 ">
+                    <RoomReservation/>
+                    </div>
                 </div>
             </div>
 
