8a2ee232a54178d33c3f1f2ff902e3cc8422f9ca
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-18 20:28:22 +0600

    private route added

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -6,6 +6,7 @@ import Login from '../pages/Login/Login'
 import SignUp from '../pages/SignUp/SignUp'
 import { TbH1 } from 'react-icons/tb'
 import RoomDetails from '../pages/RoomDetails/RoomDetails'
+import PrivateRoute from './PrivateRoute'
 
 export const router = createBrowserRouter([
   {
@@ -19,7 +20,7 @@ export const router = createBrowserRouter([
       },
       {
         path: '/room/:id',
-        element: <RoomDetails />
+        element: (<PrivateRoute><RoomDetails /></PrivateRoute>)
       },
     ],
   },
