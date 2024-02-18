529462a03ca6eac2486e140dfa6c18b1ce09435b
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-18 23:36:54 +0600

    interceptor added for unauthorized access

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,24 @@
 import axios from 'axios'
+import { clearCookie } from './auth';
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_KEY_URL,
   withCredentials: true,
+})
+
+// intercept response and check for unauthorised responses
+
+axiosSecure.interceptors.response.use(response => response, async (error) => {
+  console.log('Error tracked in interceptors', error.response);
+  if (error.response &&
+    (error.response.status === 401 || error.response.status === 403)
+  ) {
+    await clearCookie()
+    window.location.replace('/login')
+  }
+  return Promise.reject(error)
+
+
 })
 
 export default axiosSecure
