822d3f7c8075266e2604fc66a0f2b443c524939a
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-19 02:46:40 +0600

    my listing added

--- client/src/api/rooms.js
+++ client/src/api/rooms.js
@@ -1,21 +1,24 @@
 import axiosSecure from "."
-
 //fetch all rooms from db
-
 export const getAllRooms = async () => {
     const { data } = await axiosSecure('/rooms')
     return (data)
 }
-//fetch single rooms from db
+//fetch all rooms for host
+export const getHostRooms = async email => {
+    const { data } = await axiosSecure(`/rooms/${email}`)
+    return (data)
+}
 
+//fetch single rooms from db
 export const getRoom = async id => {
     const { data } = await axiosSecure(`/room/${id}`)
     return (data)
 }
 
 //save room data in db
-
 export const addRoom = async roomData => {
-    const { data } = await axiosSecure.post(`/rooms`,roomData)
+    const { data } = await axiosSecure.post(`/rooms`, roomData)
     return (data)
 }
+
--- server/index.js
+++ server/index.js
@@ -97,21 +97,29 @@ async function run() {
     })
 
     //Get all rooms
-    app.get('/rooms', async(req,res)=>{
-      const result =await roomsCollecton.find().toArray()
+    app.get('/rooms', async (req, res) => {
+      const result = await roomsCollecton.find().toArray()
       res.send(result)
     })
 
+    //get rooms for host
+    app.get('/rooms/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await roomsCollecton.find({ 'host.email': email }).toArray()
+      res.send(result)
+
+    })
+
     //get single room data
-    app.get('/room/:id', async(req,res)=>{
+    app.get('/room/:id', async (req, res) => {
       const id = req.params.id
-      const result =await roomsCollecton.findOne({_id: new ObjectId(id)})
+      const result = await roomsCollecton.findOne({ _id: new ObjectId(id) })
       res.send(result)
     })
 
     //save room in database
 
-    app.post('/rooms',verifyToken,async(req,res)=>{
+    app.post('/rooms', verifyToken, async (req, res) => {
       const room = req.body
       const result = await roomsCollecton.insertOne(room)
       res.send(result)
