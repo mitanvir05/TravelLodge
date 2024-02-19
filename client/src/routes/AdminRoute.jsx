e7d4049a859c1b3d24e38d45b9aeca0bd4913242
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-19 20:17:11 +0600

    tanstack querY added

--- client/src/routes/AdminRoute.jsx
+++ client/src/routes/AdminRoute.jsx
@@ -4,8 +4,8 @@ import Loader from "../components/Shared/Loader";
 import useRole from "../hooks/useRole";
 
 const AdminRoute = ({children}) => {
-    const [role,loading] = useRole()
-    if(loading) return <Loader/>
+    const [role,isLoading] = useRole()
+    if(isLoading) return <Loader/>
     if(role === 'admin') return children
     return <Navigate to="/dashboard" />
 };
